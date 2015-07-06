0c7614629a2887d44f9953c332df8359a17855df
Rafael Muñoz <raudo18@gmail.com>
2015-07-06 12:45:26 +0200

    Added custom property drawer to show the search scope of the dependencies.

---
+++ Editor/DependencyFieldDrawer.cs
@@ -0,0 +1,19 @@
+﻿namespace Alquimiaware.Editor
+{
+    using System.Reflection;
+    using UnityEditor;
+    using UnityEngine;
+
+    [CustomPropertyDrawer(typeof(DependencyAttribute), true)]
+    public class DependencyFieldDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var dependency = (DependencyAttribute)this.attribute;
+
+            var name = string.Format("{0} (D:{1})", label.text, dependency.SearchScope);
+
+            EditorGUI.PropertyField(position, property, new GUIContent(name), true);
+        }
+    }
+}
