401435e23b99d5365ea065d99d11dd2120963067
Elideb <elorahranma@gmail.com>
2015-08-10 03:00:21 +0200

    Added a test for a failing check: creating a dependency with default path "/Root" actually creates an empty named gameobject at the root, as "[]/Root".

---
+++ Assets/UnityDependency/Tests/CreateDefaultsTests/Capture_Defaults_CreatedInRootWithSlash.cs
@@ -0,0 +1,37 @@
+﻿namespace UnityDependency.Test.CreateDefaultsTests
+{
+    using UnityEngine;
+    using UnityHelpers;
+    using Alquimiaware;
+    using AssertExtensions;
+
+    [IntegrationTest.DynamicTest("CreateDefaultsTests")]
+    public class Capture_Defaults_CreatedInRootWithSlash : MonoBehaviour
+    {
+        private CaptureSampleDefaultPathWithSlash testSubject = null;
+
+        void Start()
+        {
+            this.testSubject = new GameObject("Subject").AddComponent<CaptureSampleDefaultPathWithSlash>();
+        }
+
+        void Update()
+        {
+            try
+            {
+                this.testSubject.CaptureDependencies();
+
+                this.testSubject.RootCollider.gameObject.AssertIsTree("Root");
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                DestroyImmediate(this.testSubject.RootCollider.gameObject);
+                DestroyImmediate(this.testSubject.gameObject);
+            }
+        }
+    }
+}
