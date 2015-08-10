c54569593320301d67f7c395efe2a2d83fdb8591
Elideb <elorahranma@gmail.com>
2015-08-10 02:20:18 +0200

    Added helper to create hierarchies, plus extensions to perform different asserts on Object, Component or GameObject directly.

---
+++ Assets/UnityDependency/Tests/TestExtensions/UnityHelpers.cs
@@ -0,0 +1,20 @@
+﻿namespace UnityDependency.Test.UnityHelpers
+{
+    using UnityEngine;
+
+    public static class UnityHelpers
+    {
+        public static GameObject[] CreateHierarchy(params string[] names)
+        {
+            GameObject[] created = new GameObject[names.Length];
+            for(int i = 0; i < names.Length; ++i)
+            {
+                created[i] = new GameObject(names[i]);
+                if (i > 0)
+                    created[i].transform.parent = created[i - 1].transform;
+            }
+
+            return created;
+        }
+    }
+}
