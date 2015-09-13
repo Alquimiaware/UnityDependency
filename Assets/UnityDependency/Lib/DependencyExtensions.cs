6e6fadee7bfc357437a91e0e0da71a8311abba1a
Elideb <elorahranma@gmail.com>
2015-09-13 02:12:26 +0200

    Fix: private fields from base classes were not captured. GetFields obtains all fields from base classes.

--- Assets/UnityDependency/Lib/DependencyExtensions.cs
+++ Assets/UnityDependency/Lib/DependencyExtensions.cs
@@ -1,6 +1,7 @@
 namespace Alquimiaware
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
     using UnityEngine;
@@ -394,10 +395,18 @@ namespace Alquimiaware
 
         private static FieldInfo[] GetFields(MonoBehaviour monoBehaviour)
         {
-            var type = monoBehaviour.GetType();
+            return GetFields(monoBehaviour.GetType()).ToArray();
+        }
 
+        private static IEnumerable<FieldInfo> GetFields(Type type)
+        {
             var fieldInfos = type.GetFields(
                 BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+
+            Type baseType = type.BaseType;
+            if (baseType != null)
+                return fieldInfos.Concat(GetFields(baseType)).Distinct();
+
             return fieldInfos;
         }
 
