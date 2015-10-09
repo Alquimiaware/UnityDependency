9d206959b5890b3c250dffa18952de370698d4db
Elideb <elorahranma@gmail.com>
2015-10-09 23:18:58 +0200

    Created tests for default creation, when DefaultType is or is not defined.

---
+++ Assets/UnityDependency/Tests/Editor/Defaults/DefaultType.cs
@@ -0,0 +1,49 @@
+﻿namespace UnityDependency.Tests
+{
+    using Alquimiaware;
+    using NUnit.Framework;
+    using UnityEngine;
+
+    public abstract partial class DependencyExtension_ParentChain
+    {
+        public class DefaultType : DependencyExtension_ParentChain
+        {
+            [Test]
+            public void Capture_HasNoDefaultType_IsCaptured()
+            {
+                var sut = this.goBuilder.CreateInRoot<DefaultTypeNotDeclared>();
+                sut.CaptureDependencies();
+
+                Assert.IsNotNull(sut.field);
+            }
+
+            [Test]
+            public void Capture_HasNoDefaultType_CreatesComponentOfFieldType()
+            {
+                var sut = this.goBuilder.CreateInRoot<DefaultTypeNotDeclared>();
+                sut.CaptureDependencies();
+
+                // Is.TypeOf checks exact type, whereas Assert.IsInstanceOf<> checks type compatibility
+                Assert.That(sut.field, Is.TypeOf<CapsuleCollider>());
+            }
+
+            [Test]
+            public void Capture_HasDefaultType_IsCaptured()
+            {
+                var sut = this.goBuilder.CreateInRoot<DefaultTypeDeclared>();
+                sut.CaptureDependencies();
+
+                Assert.IsNotNull(sut.field);
+            }
+
+            [Test]
+            public void Capture_HasDefaultType_CreatesComponentOfType()
+            {
+                var sut = this.goBuilder.CreateInRoot<DefaultTypeDeclared>();
+                sut.CaptureDependencies();
+
+                Assert.That(sut.field, Is.TypeOf<CapsuleCollider>());
+            }
+        }
+    }
+}
---
+++ Assets/UnityDependency/Tests/Editor/Defaults/DependencyExtension_Defaults.cs
@@ -0,0 +1,22 @@
+﻿namespace UnityDependency.Tests
+{
+    using Alquimiaware;
+    using NUnit.Framework;
+    using UnityEngine;
+
+    [TestFixture]
+    public abstract partial class DependencyExtension_ParentChain
+    {
+        public class DefaultTypeNotDeclared : MonoBehaviour
+        {
+            [Dependency(Scope.Scene)]
+            public CapsuleCollider field = null;
+        }
+
+        public class DefaultTypeDeclared : MonoBehaviour
+        {
+            [Dependency(Scope.Scene, DefaultType = typeof(CapsuleCollider))]
+            public Collider field = null;
+        }
+    }
+}
